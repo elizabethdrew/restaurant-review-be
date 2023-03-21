102d1905bccbbebdff2926f4030b4d53de9055c7
dreweb <elizabeth.drew@coutts.com>
2023-03-21 12:12:50 +0000

    /restaurants addNewRestaurant adds to database and returns correctly

--- src/main/java/dev/drew/restaurantreview/controller/RestaurantController.java
+++ src/main/java/dev/drew/restaurantreview/controller/RestaurantController.java
@@ -2,12 +2,18 @@ package dev.drew.restaurantreview.controller;
 
 import dev.drew.restaurantreview.entity.Restaurant;
 import dev.drew.restaurantreview.repository.RestaurantRepository;
+
+import org.openapitools.model.Error;
 import org.openapitools.model.RestaurantInput;
 import org.openapitools.model.RestaurantResponse;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 
 @RestController
@@ -26,9 +32,31 @@ public class RestaurantController implements org.openapitools.api.RestaurantsApi
         restaurant.setName(restaurantInput.getName());
         restaurant.setCity(restaurantInput.getCity());
         restaurant.setRating(restaurantInput.getRating());
+        restaurant.setCreatedAt(OffsetDateTime.now());
 
-        restaurantRepository.save(restaurant);
-        return null;
+        RestaurantResponse restaurantResponse = new RestaurantResponse();
+
+        try {
+            Restaurant savedRestaurant = restaurantRepository.save(restaurant);
+            restaurantResponse.setRestaurant(savedRestaurant);
+            restaurantResponse.setSuccess(true);
+            return new ResponseEntity<>(restaurantResponse, HttpStatus.CREATED);
+        } catch (DataIntegrityViolationException e) {
+            // Handle database constraint violations, such as unique constraints
+            restaurantResponse.setSuccess(false);
+            restaurantResponse.setError(new Error().message("Invalid input: " + e.getMessage()));
+            return new ResponseEntity<>(restaurantResponse, HttpStatus.BAD_REQUEST);
+        } catch (DataAccessException e) {
+            // Handle other database-related exceptions
+            restaurantResponse.setSuccess(false);
+            restaurantResponse.setError(new Error().message("Database error: " + e.getMessage()));
+            return new ResponseEntity<>(restaurantResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (Exception e) {
+            // Handle general exceptions
+            restaurantResponse.setSuccess(false);
+            restaurantResponse.setError(new Error().message("An unexpected error occurred: " + e.getMessage()));
+            return new ResponseEntity<>(restaurantResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Override
