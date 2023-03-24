51570534492a5aef90292d80f3547a4416503fec
dreweb <elizabeth.drew@coutts.com>
2023-03-24 16:03:34 +0000

    Basic security setup using method security to make sure only admin can add, delete or edit restaurants. Could possibly move the endpoint authorisation to the SecurityFilterChain http in SecurityConfig?

--- src/main/java/dev/drew/restaurantreview/config/SecurityConfig.java
+++ src/main/java/dev/drew/restaurantreview/config/SecurityConfig.java
@@ -2,12 +2,9 @@ package dev.drew.restaurantreview.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
