1bd0b1adeb10a7897971d8927e2a6972eb90a243
dreweb <elizabeth.drew@coutts.com>
2023-04-06 11:39:51 +0100

    RestaurantServiceImpl comments added

--- src/main/java/dev/drew/restaurantreview/service/RestaurantServiceImpl.java
+++ src/main/java/dev/drew/restaurantreview/service/RestaurantServiceImpl.java
@@ -26,11 +26,13 @@ import static dev.drew.restaurantreview.util.SecurityUtils.*;
 @Service
 public class RestaurantServiceImpl implements RestaurantService {
 
+    // Instance variables for the RestaurantRepository, ReviewRepository, RestaurantMapper, and EntityUserIdProvider
     private final RestaurantRepository restaurantRepository;
     private final ReviewRepository reviewRepository;
     private final RestaurantMapper restaurantMapper;
     private final EntityUserIdProvider<RestaurantEntity> restaurantUserIdProvider = RestaurantEntity::getUserId;
 
+    // Constructor with required dependencies
     public RestaurantServiceImpl(RestaurantRepository restaurantRepository, RestaurantMapper restaurantMapper, ReviewRepository reviewRepository) {
         this.restaurantRepository = restaurantRepository;
         this.restaurantMapper = restaurantMapper;
@@ -40,18 +42,24 @@ public class RestaurantServiceImpl implements RestaurantService {
     // Add a new restaurant to the database
     public ResponseEntity<RestaurantResponse> addNewRestaurant(RestaurantInput restaurantInput) {
 
+        // Convert the input data to a RestaurantEntity object, set the created timestamp, and current user ID
         RestaurantEntity restaurant = restaurantMapper.toRestaurantEntity(restaurantInput);
         restaurant.setCreatedAt(OffsetDateTime.now());
         Long currentUserId = getCurrentUserId();
         restaurant.setUserId(currentUserId);
 
+        // Initialize the RestaurantResponse object
         RestaurantResponse restaurantResponse = new RestaurantResponse();
 
         try {
+            // Save the new restaurant to the database
             RestaurantEntity savedRestaurant = restaurantRepository.save(restaurant);
+            // Convert the saved RestaurantEntity object to a Restaurant object
             Restaurant savedApiRestaurant = restaurantMapper.toRestaurant(savedRestaurant);
+            // Set the response fields
             restaurantResponse.setRestaurant(savedApiRestaurant);
             restaurantResponse.setSuccess(true);
+            // Return the response with the CREATED status code
             return new ResponseEntity<>(restaurantResponse, HttpStatus.CREATED);
         } catch (DataIntegrityViolationException e) {
             // Handle database constraint violations, such as unique constraints
@@ -71,10 +79,10 @@ public class RestaurantServiceImpl implements RestaurantService {
         }
     }
 
-
     // Get all restaurants from the database
     public ResponseEntity<List<Restaurant>> getAllRestaurants(String city, Integer rating, Long userId) {
 
+        // Get a stream of all restaurants and apply filters if provided
         Stream<RestaurantEntity> filteredEntities = restaurantRepository.findAll().stream();
 
         if (city != null) {
@@ -89,30 +97,34 @@ public class RestaurantServiceImpl implements RestaurantService {
             filteredEntities = filteredEntities.filter(r -> r.getUserId().equals(userId));
         }
 
+        // Convert the filtered RestaurantEntity objects to Restaurant objects and return them as a list
         List<Restaurant> restaurants = filteredEntities
                 .map(restaurantMapper::toRestaurant)
                 .collect(Collectors.toList());
         return ResponseEntity.ok(restaurants);
     }
 
-
     // Get a restaurant by ID
     public ResponseEntity<Restaurant> getRestaurantById(Integer restaurantId) {
+        // Retrieve the restaurant with the specified ID from the repository
         Optional<RestaurantEntity> restaurantEntityOptional = restaurantRepository.findById(restaurantId.longValue());
 
+        // If the restaurant exists, convert it to a Restaurant object and return it
         if (restaurantEntityOptional.isPresent()) {
             Restaurant restaurant = restaurantMapper.toRestaurant(restaurantEntityOptional.get());
             return ResponseEntity.ok(restaurant);
         } else {
+            // If the restaurant does not exist, return a NOT_FOUND status
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
 
-
     // Update a restaurant by ID
     public ResponseEntity<Restaurant> updateRestaurantById(Integer restaurantId, RestaurantInput restaurantInput) {
+        // Retrieve the restaurant with the specified ID from the repository
         Optional<RestaurantEntity> restaurantEntityOptional = restaurantRepository.findById(restaurantId.longValue());
 
+        // If the restaurant exists, proceed with the update
         if (restaurantEntityOptional.isPresent()) {
             RestaurantEntity restaurantEntity = restaurantEntityOptional.get();
 
@@ -121,14 +133,18 @@ public class RestaurantServiceImpl implements RestaurantService {
                 return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
             }
 
+            // Convert the input data to a RestaurantEntity object and set its ID, created timestamp, and user ID
             RestaurantEntity updatedEntity = restaurantMapper.toRestaurantEntity(restaurantInput);
             updatedEntity.setId(restaurantEntity.getId());
             updatedEntity.setCreatedAt(restaurantEntity.getCreatedAt());
             updatedEntity.setUserId(restaurantEntity.getUserId());
 
             try {
+                // Save the updated restaurant to the database
                 RestaurantEntity savedRestaurant = restaurantRepository.save(updatedEntity);
+                // Convert the saved RestaurantEntity object to a Restaurant object
                 Restaurant savedApiRestaurant = restaurantMapper.toRestaurant(savedRestaurant);
+                // Return the updated restaurant
                 return ResponseEntity.ok(savedApiRestaurant);
             } catch (DataIntegrityViolationException e) {
                 // Handle database constraint violations, such as unique constraints
@@ -141,15 +157,17 @@ public class RestaurantServiceImpl implements RestaurantService {
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
             }
         } else {
+            // If the restaurant does not exist, return a NOT_FOUND status
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
 
-
     // Delete a restaurant by ID
     public ResponseEntity<Void> deleteRestaurantById(Integer restaurantId) {
+        // Retrieve the restaurant with the specified ID from the repository
         Optional<RestaurantEntity> restaurantEntityOptional = restaurantRepository.findById(restaurantId.longValue());
 
+        // If the restaurant exists, proceed with the deletion
         if (restaurantEntityOptional.isPresent()) {
             RestaurantEntity restaurantEntity = restaurantEntityOptional.get();
 
@@ -158,9 +176,11 @@ public class RestaurantServiceImpl implements RestaurantService {
                 return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
             }
 
+            // Delete the restaurant by ID
             restaurantRepository.deleteById(restaurantId.longValue());
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         } else {
+            // If the restaurant does not exist, return a NOT_FOUND status
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
