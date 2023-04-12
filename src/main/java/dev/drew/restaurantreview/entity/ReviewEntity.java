de8b98fad116aa9f9867fdcfdc97749bab12afc9
dreweb <elizabeth.drew@coutts.com>
2023-04-12 18:58:43 +0100

    The Entity files are causing the issue - adding lombok back fixed the user null issue - need to dig deeper on how to fix that without lombok. When new restaurant being added, user entity and restaurant entity are null.

--- src/main/java/dev/drew/restaurantreview/entity/ReviewEntity.java
+++ src/main/java/dev/drew/restaurantreview/entity/ReviewEntity.java
@@ -24,13 +24,11 @@ public class ReviewEntity extends org.openapitools.model.Review {
     private Long id;
 
     @NotNull
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "restaurant_id", nullable = false)
-    private RestaurantEntity restaurantEntity;
+    @Column(name = "restaurant_id")
+    private Long restaurantId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private UserEntity userEntity;
+    @Column(name = "user_id")
+    private Long userId;
 
     @NotNull
     @Column(name = "rating")
@@ -47,100 +45,4 @@ public class ReviewEntity extends org.openapitools.model.Review {
     @Column(name = "updated_at")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private OffsetDateTime updatedAt;
-
-    public RestaurantEntity getrestaurantEntity() {
-        return restaurantEntity;
-    }
-
-    public void setRestaurantEntity(RestaurantEntity restaurantEntity) {
-        this.restaurantEntity = restaurantEntity;
-    }
-
-    @Override
-    public Long getRestaurantId() {
-        return restaurantEntity != null ? restaurantEntity.getId() : null;
-    }
-
-    @Override
-    public void setRestaurantId(Long restaurantId) {
-        if (restaurantEntity == null) {
-            restaurantEntity = new RestaurantEntity();
-        }
-        restaurantEntity.setId(restaurantId);
-    }
-
-    public UserEntity getUserEntity() {
-        return userEntity;
-    }
-
-    public void setUserEntity(UserEntity userEntity) {
-        this.userEntity = userEntity;
-    }
-
-    @Override
-    public Long getUserId() {
-        return userEntity != null ? userEntity.getId() : null;
-    }
-
-    @Override
-    public void setUserId(Long userId) {
-        if (userEntity == null) {
-            userEntity = new UserEntity();
-        }
-        userEntity.setId(userId);
-    }
-
-    @Override
-    public Long getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public RestaurantEntity getRestaurantEntity() {
-        return restaurantEntity;
-    }
-
-    @Override
-    public Float getRating() {
-        return rating;
-    }
-
-    @Override
-    public void setRating(Float rating) {
-        this.rating = rating;
-    }
-
-    @Override
-    public String getComment() {
-        return comment;
-    }
-
-    @Override
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    @Override
-    public OffsetDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    @Override
-    public void setCreatedAt(OffsetDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    @Override
-    public OffsetDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    @Override
-    public void setUpdatedAt(OffsetDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
