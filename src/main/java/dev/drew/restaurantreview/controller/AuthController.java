9c091f394fc49bf0eae2bb6fe9f4737bee3260fc
dreweb <elizabeth.drew@coutts.com>
2023-04-12 16:48:29 +0100

    Updated AuthController

--- src/main/java/dev/drew/restaurantreview/controller/AuthController.java
+++ src/main/java/dev/drew/restaurantreview/controller/AuthController.java
@@ -1,11 +1,14 @@
 package dev.drew.restaurantreview.controller;
 
+import dev.drew.restaurantreview.entity.LoginRequest;
 import dev.drew.restaurantreview.service.TokenService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -23,11 +26,9 @@ public class AuthController {
     }
 
     @PostMapping("/token")
-    public String token() {
-        LOG.debug("Token requested for user: '{}'", authentication.getName());
-        String token = tokenService.generateToken(authentication);
-        LOG.debug("Token granted: {}", token);
-        return token;
+    public String token(@RequestBody LoginRequest userLogin) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userLogin.username(), userLogin.password()));
+        return tokenService.generateToken(authentication);
     }
 
 }
