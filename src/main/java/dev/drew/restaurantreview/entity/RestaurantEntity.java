fa6d0a5229efcef43a72237dee8648a9617217b6
dreweb <elizabeth.drew@coutts.com>
2023-04-13 16:00:31 +0100

    Changed the RestaurantEntity to remove lombok

--- src/main/java/dev/drew/restaurantreview/entity/RestaurantEntity.java
+++ src/main/java/dev/drew/restaurantreview/entity/RestaurantEntity.java
@@ -1,16 +1,13 @@
 package dev.drew.restaurantreview.entity;
 
 import java.time.OffsetDateTime;
+import java.util.Objects;
 import javax.validation.constraints.*;
-import lombok.*;
 import jakarta.persistence.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name = "restaurant")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class RestaurantEntity extends org.openapitools.model.Restaurant {
 
     @Id
@@ -29,14 +26,91 @@ public class RestaurantEntity extends org.openapitools.model.Restaurant {
     @Column(name = "user_id")
     private Long userId;
 
-     @NotNull
-     @Min(1)
-     @Max(5)
-     @Column(name = "rating")
-     private Integer rating;
+    @NotNull
+    @Min(1)
+    @Max(5)
+    @Column(name = "rating")
+    private Integer rating;
 
     @Column(name = "created_at")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private OffsetDateTime createdAt;
 
+    public RestaurantEntity() {
+    }
+
+    public RestaurantEntity(Long id, String name, String city, Long userId, Integer rating, OffsetDateTime createdAt) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+        this.userId = userId;
+        this.rating = rating;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RestaurantEntity that = (RestaurantEntity) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(city, that.city) &&
+                Objects.equals(userId, that.userId) &&
+                Objects.equals(rating, that.rating) &&
+                Objects.equals(createdAt, that.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, city, userId, rating, createdAt);
+    }
 }
