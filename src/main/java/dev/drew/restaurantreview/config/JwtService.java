fcca6b29ab1ee936ca2e8d4660e64a91a7870a77
dreweb <elizabeth.drew@coutts.com>
2023-04-13 11:54:21 +0100

    generateToken created

--- src/main/java/dev/drew/restaurantreview/config/JwtService.java
+++ src/main/java/dev/drew/restaurantreview/config/JwtService.java
@@ -2,11 +2,15 @@ package dev.drew.restaurantreview.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -21,6 +25,17 @@ public class JwtService {
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(Map<String, Object> extractClaims, UserDetails userDetails) { //might need to use securityuser?
+        return Jwts
+                .builder()
+                .setClaims(extractClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
     private Claims extractAllClaims(String token) {
