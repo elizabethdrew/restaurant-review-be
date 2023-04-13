4be8b31e601b2bd6151fd3d3636e730655b6131f
dreweb <elizabeth.drew@coutts.com>
2023-04-13 11:44:02 +0100

    extractAllClaims created

--- src/main/java/dev/drew/restaurantreview/config/JwtService.java
+++ src/main/java/dev/drew/restaurantreview/config/JwtService.java
@@ -2,10 +2,16 @@ package dev.drew.restaurantreview.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "472B4B6250645367566B5970337336763979244226452948404D635166546A57";
 
     public String extractUsername(String token) {
         return null;
@@ -14,9 +20,14 @@ public class JwtService {
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
-                .setSigningKey(getSignInKey)
+                .setSigningKey(getSignInKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Key getSignInKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
