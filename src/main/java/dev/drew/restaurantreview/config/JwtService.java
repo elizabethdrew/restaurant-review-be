ea2e9b41bd3e7c5a2a553827703603a783db18a1
dreweb <elizabeth.drew@coutts.com>
2023-04-13 11:24:15 +0100

    Created JwtService

---
+++ src/main/java/dev/drew/restaurantreview/config/JwtService.java
@@ -0,0 +1,11 @@
+package dev.drew.restaurantreview.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUsername(String token) {
+        return null;
+    }
+}
