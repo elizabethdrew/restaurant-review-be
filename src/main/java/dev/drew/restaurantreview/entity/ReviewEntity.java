d8d34f03f840db9d0505e9e62e9378eb2bed2064
dreweb <elizabeth.drew@coutts.com>
2023-04-13 16:05:52 +0100

    Changed the ReviewEntity to remove lombok

--- src/main/java/dev/drew/restaurantreview/entity/ReviewEntity.java
+++ src/main/java/dev/drew/restaurantreview/entity/ReviewEntity.java
@@ -1,20 +1,14 @@
 package dev.drew.restaurantreview.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotNull;
 import java.time.OffsetDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "review")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ReviewEntity extends org.openapitools.model.Review {
 
     @Id
@@ -45,4 +39,91 @@ public class ReviewEntity extends org.openapitools.model.Review {
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private OffsetDateTime updatedAt;
 
+    public ReviewEntity() {
+    }
+
+    public ReviewEntity(Long id, Long restaurantId, Long userId, Float rating, String comment, OffsetDateTime createdAt, OffsetDateTime updatedAt) {
+        this.id = id;
+        this.restaurantId = restaurantId;
+        this.userId = userId;
+        this.rating = rating;
+        this.comment = comment;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(Long restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Float getRating() {
+        return rating;
+    }
+
+    public void setRating(Float rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public OffsetDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(OffsetDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ReviewEntity that = (ReviewEntity) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(restaurantId, that.restaurantId) &&
+                Objects.equals(userId, that.userId) &&
+                Objects.equals(rating, that.rating) &&
+                Objects.equals(comment, that.comment) &&
+                Objects.equals(createdAt, that.createdAt) &&
+                Objects.equals(updatedAt, that.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, restaurantId, userId, rating, comment, createdAt, updatedAt);
+    }
 }
