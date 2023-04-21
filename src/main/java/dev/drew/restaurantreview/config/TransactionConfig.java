386e18d3e73fa1bceaa583d6ddd95f152a6efa9b
dreweb <elizabeth.drew@coutts.com>
2023-04-21 10:43:56 +0100

    Added TransactionConfig

---
+++ src/main/java/dev/drew/restaurantreview/config/TransactionConfig.java
@@ -0,0 +1,18 @@
+package dev.drew.restaurantreview.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class TransactionConfig {
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+}
