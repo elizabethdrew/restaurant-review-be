66da0548b4ca996182ece5cdc8a405f9425050a5
dreweb <elizabeth.drew@coutts.com>
2023-04-21 11:57:44 +0100

    SecurityConfig unit test updated

--- src/main/java/dev/drew/restaurantreview/config/SecurityConfig.java
+++ src/main/java/dev/drew/restaurantreview/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -58,8 +57,6 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 // Configure the response headers to allow frame options from the same origin
                 .headers(headers -> headers.frameOptions().sameOrigin())
-                // Enable basic HTTP authentication
-                //.httpBasic(Customizer.withDefaults())
                 .build();
     }
 }
--- src/test/java/dev/drew/restaurantreview/config/SecurityConfigTest.java
+++ src/test/java/dev/drew/restaurantreview/config/SecurityConfigTest.java
@@ -1,41 +1,37 @@
 package dev.drew.restaurantreview.config;
 
-import dev.drew.restaurantreview.auth.ApplicationConfig;
+import dev.drew.restaurantreview.auth.JwtAuthenticationFilter;
+import dev.drew.restaurantreview.service.JpaUserDetailsService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-// Use the SpringExtension for JUnit 5 to integrate with Spring Boot
 @ExtendWith(SpringExtension.class)
-// Load the Spring Boot application context to create necessary beans
 @SpringBootTest
-public class SecurityConfigTest {
+class SecurityConfigTest {
 
     @Autowired
+    private JpaUserDetailsService jpaUserDetailsService;
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthFilter;
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
+
     private SecurityConfig securityConfig;
 
-    private HttpSecurity httpSecurity;
-
-    // Test the passwordEncoder() method to ensure it returns a non-null BCryptPasswordEncoder instance
-    @Test
-    public void testPasswordEncoder() {
-        BCryptPasswordEncoder encoder = ApplicationConfig.passwordEncoder();
-        assertNotNull(encoder, "PasswordEncoder should not be null");
-        assertTrue(encoder instanceof BCryptPasswordEncoder, "PasswordEncoder should be an instance of BCryptPasswordEncoder");
+    @BeforeEach
+    void setUp() {
+        securityConfig = new SecurityConfig(jpaUserDetailsService, jwtAuthFilter, authenticationProvider);
     }
 
-    // Test the securityFilterChain(HttpSecurity) method to ensure it returns a non-null SecurityFilterChain
     @Test
-    public void testSecurityFilterChain() throws Exception {
-        SecurityFilterChain securityFilterChain = securityConfig.securityFilterChain(httpSecurity);
-        assertNotNull(securityFilterChain, "SecurityFilterChain should not be null");
+    void testConstructorInjection() {
+        assertNotNull(securityConfig, "SecurityConfig constructor injection failed");
     }
 }
