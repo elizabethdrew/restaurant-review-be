15d7a84ff760f9f562edad6ea07f75a19c12db56
dreweb <elizabeth.drew@coutts.com>
2023-05-12 11:59:02 +0100

    Changed to Lombok logging

--- src/main/java/dev/drew/restaurantreview/controller/GlobalExceptionHandler.java
+++ src/main/java/dev/drew/restaurantreview/controller/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package dev.drew.restaurantreview.controller;
 
 import dev.drew.restaurantreview.exception.InsufficientPermissionException;
 import dev.drew.restaurantreview.exception.RestaurantNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
@@ -9,19 +10,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     // Handle RestaurantNotFoundException
     @ExceptionHandler(RestaurantNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<?> handleRestaurantNotFoundException(RestaurantNotFoundException e) {
-        logger.warn("Restaurant not found error", e);
+        log.warn("Restaurant not found error", e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
@@ -29,7 +28,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(InsufficientPermissionException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ResponseEntity<?> handleInsufficientPermissionException(InsufficientPermissionException e) {
-        logger.warn("Insufficient permission error", e);
+        log.warn("Insufficient permission error", e);
         return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     }
 
@@ -37,7 +36,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<?> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
-        logger.warn("Data integrity error", e);
+        log.warn("Data integrity error", e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
@@ -45,7 +44,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(DataAccessException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<?> handleDataAccessException(DataAccessException e) {
-        logger.warn("Data access error", e);
+        log.warn("Data access error", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 
@@ -53,7 +52,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<?> handleGeneralException(Exception e) {
-        logger.warn("An unexpected error occurred", e);
+        log.warn("An unexpected error occurred", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 }
