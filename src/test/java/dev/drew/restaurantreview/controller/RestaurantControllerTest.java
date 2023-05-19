af5e8396e0aaf888f07d36746229e8a98bd31095
dreweb <elizabeth.drew@coutts.com>
2023-05-19 10:20:53 +0100

    RestaurantControllerTest updated

--- src/test/java/dev/drew/restaurantreview/controller/RestaurantControllerTest.java
+++ src/test/java/dev/drew/restaurantreview/controller/RestaurantControllerTest.java
@@ -1,6 +1,8 @@
 package dev.drew.restaurantreview.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.drew.restaurantreview.exception.InsufficientPermissionException;
+import dev.drew.restaurantreview.exception.RestaurantNotFoundException;
 import dev.drew.restaurantreview.service.RestaurantService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,17 +12,15 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.openapitools.model.Restaurant;
 import org.openapitools.model.RestaurantInput;
-import org.openapitools.model.RestaurantResponse;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -43,18 +43,14 @@ class RestaurantControllerTest {
 
     @Test
     void testAddNewRestaurant() throws Exception {
-        RestaurantResponse restaurantResponse = new RestaurantResponse();
-
-        // Prepare input data and expected response
         RestaurantInput input = new RestaurantInput().name("New Restaurant").city("New City");
         Restaurant restaurant = new Restaurant().id(1L).name(input.getName()).city(input.getCity());
-        ResponseEntity<RestaurantResponse> responseEntity = ResponseEntity.status(HttpStatus.CREATED).body(restaurantResponse);
 
         // Mock the service call
-        when(restaurantService.addNewRestaurant(any(RestaurantInput.class))).thenReturn(responseEntity);
+        when(restaurantService.addNewRestaurant(any(RestaurantInput.class))).thenReturn(restaurant);
 
         // Call the endpoint and check the response status
-        mockMvc.perform(post("/api/v1/restaurants")
+        mockMvc.perform(post("/api/v1/restaurant/add")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(input)))
                 .andExpect(status().isCreated());
@@ -62,15 +58,12 @@ class RestaurantControllerTest {
 
     @Test
     void testGetAllRestaurants() throws Exception {
-        List<Restaurant> restaurants = new ArrayList<>();
-        Restaurant restaurant1 = new Restaurant().id(1L).name("Restaurant 1").city("City 1");
-        Restaurant restaurant2 = new Restaurant().id(2L).name("Restaurant 2").city("City 2");
-        restaurants.add(restaurant1);
-        restaurants.add(restaurant2);
+        List<Restaurant> restaurants = Arrays.asList(
+                new Restaurant().id(1L).name("Restaurant 1").city("City 1"),
+                new Restaurant().id(2L).name("Restaurant 2").city("City 2")
+        );
 
-        ResponseEntity<List<Restaurant>> responseEntity = ResponseEntity.ok(restaurants);
-
-        when(restaurantService.getAllRestaurants(null, null, null)).thenReturn(responseEntity);
+        when(restaurantService.getAllRestaurants(null, null, null)).thenReturn(restaurants);
 
         mockMvc.perform(get("/api/v1/restaurants")
                         .contentType(MediaType.APPLICATION_JSON))
@@ -79,29 +72,27 @@ class RestaurantControllerTest {
 
     @Test
     void testGetRestaurantById() throws Exception {
-        Long restaurantId = 1L;
-        Restaurant restaurant = new Restaurant().id(restaurantId).name("Test Restaurant").city("Test City");
+        int restaurantId = 1;
+        Restaurant restaurant = new Restaurant().id((long) restaurantId).name("Test Restaurant").city("Test City");
 
-        ResponseEntity<Restaurant> responseEntity = ResponseEntity.ok(restaurant);
+        when(restaurantService.getRestaurantById(eq(restaurantId))).thenReturn(restaurant);
 
-        when(restaurantService.getRestaurantById(restaurantId.intValue())).thenReturn(responseEntity);
-
-        mockMvc.perform(get("/api/v1/restaurants/{restaurantId}", restaurantId)
+        mockMvc.perform(get("/api/v1/restaurant/{restaurantId}", restaurantId)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
     @Test
     void testUpdateRestaurantById() throws Exception {
-        Long restaurantId = 1L;
+        int restaurantId = 1;
         RestaurantInput input = new RestaurantInput().name("Updated Restaurant").city("Updated City");
-        Restaurant updatedRestaurant = new Restaurant().id(restaurantId).name(input.getName()).city(input.getCity());
+        Restaurant updatedRestaurant = new Restaurant().id((long) restaurantId).name(input.getName()).city(input.getCity());
 
-        ResponseEntity<Restaurant> responseEntity = ResponseEntity.ok(updatedRestaurant);
+        when(restaurantService.updateRestaurantById(eq(restaurantId), eq(input))).thenReturn(updatedRestaurant);
 
-        when(restaurantService.updateRestaurantById(restaurantId.intValue(), input)).thenReturn(responseEntity);
+        mockMvc
 
-        mockMvc.perform(put("/api/v1/restaurants/{restaurantId}", restaurantId)
+                .perform(put("/api/v1/restaurant/{restaurantId}/edit", restaurantId)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(input)))
                 .andExpect(status().isOk());
@@ -109,13 +100,9 @@ class RestaurantControllerTest {
 
     @Test
     void testDeleteRestaurantById() throws Exception {
-        Long restaurantId = 1L;
+        int restaurantId = 1;
 
-        ResponseEntity<Void> responseEntity = ResponseEntity.noContent().build();
-
-        when(restaurantService.deleteRestaurantById(restaurantId.intValue())).thenReturn(responseEntity);
-
-        mockMvc.perform(delete("/api/v1/restaurants/{restaurantId}", restaurantId)
+        mockMvc.perform(delete("/api/v1/restaurant/{restaurantId}/delete", restaurantId)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNoContent());
     }
