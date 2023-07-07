d44b19208a4e1482f1bb34f36ecc4d3dde2d247c
dreweb <elizabeth.drew@coutts.com>
2023-07-07 14:59:48 +0100

    Added note about JPA Criteria Queries being used

--- src/main/java/dev/drew/restaurantreview/repository/RestaurantRepository.java
+++ src/main/java/dev/drew/restaurantreview/repository/RestaurantRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 // The RestaurantRepository interface is responsible for interacting with the database to manage RestaurantEntity objects.
@@ -15,3 +14,7 @@ public interface RestaurantRepository extends JpaRepository<RestaurantEntity, Lo
     Optional<RestaurantEntity> findByNameAndCity(String name, String city);
 
 }
+
+/*
+This code uses JPA Criteria Queries https://www.baeldung.com/hibernate-criteria-queries
+ */
