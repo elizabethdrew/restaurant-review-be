838c76f999de8fede25d2e29eec719e4ef4e42b4
dreweb <elizabeth.drew@coutts.com>
2023-08-16 17:57:59 +0100

    Matching to OpenApi spec

--- src/main/java/dev/drew/restaurantreview/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/drew/restaurantreview/exception/GlobalExceptionHandler.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.mail.AuthenticationFailedException;
 
 @Slf4j
 @ControllerAdvice
