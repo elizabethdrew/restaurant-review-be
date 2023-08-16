8437dc1fd1251f072840743dd30d77882015bb3d
dreweb <elizabeth.drew@coutts.com>
2023-08-16 20:20:52 +0100

    Tidy

--- src/main/java/dev/drew/restaurantreview/repository/ReviewRepository.java
+++ src/main/java/dev/drew/restaurantreview/repository/ReviewRepository.java
@@ -1,23 +1,14 @@
 package dev.drew.restaurantreview.repository;
 
-import dev.drew.restaurantreview.entity.RestaurantEntity;
 import dev.drew.restaurantreview.entity.ReviewEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 // The ReviewRepository interface is responsible for interacting with the database to manage ReviewEntity objects.
 @Repository
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long>, JpaSpecificationExecutor<ReviewEntity> {
-
-
 }
 
 /*
