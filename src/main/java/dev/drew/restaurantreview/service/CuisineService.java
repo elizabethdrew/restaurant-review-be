9dd8d56af90fe39b195736b09ca7c4b02b7533f9
dreweb <elizabeth.drew@coutts.com>
2023-09-22 11:25:37 +0100

    Updated Service Imports

--- src/main/java/dev/drew/restaurantreview/service/CuisineService.java
+++ src/main/java/dev/drew/restaurantreview/service/CuisineService.java
@@ -1,8 +1,6 @@
 package dev.drew.restaurantreview.service;
 
 import dev.drew.restaurantreview.entity.CuisineEntity;
-import org.openapitools.model.Cuisine;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
