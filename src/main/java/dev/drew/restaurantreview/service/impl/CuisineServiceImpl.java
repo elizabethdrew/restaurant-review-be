5426246b334c094e2f717776ae165cb38d6cbd06
dreweb <elizabeth.drew@coutts.com>
2023-09-22 11:24:40 +0100

    Updated ServiceImpl Imports

--- src/main/java/dev/drew/restaurantreview/service/impl/CuisineServiceImpl.java
+++ src/main/java/dev/drew/restaurantreview/service/impl/CuisineServiceImpl.java
@@ -9,8 +9,6 @@ import dev.drew.restaurantreview.exception.InsufficientPermissionException;
 import dev.drew.restaurantreview.repository.CuisineRepository;
 import dev.drew.restaurantreview.service.CuisineService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
