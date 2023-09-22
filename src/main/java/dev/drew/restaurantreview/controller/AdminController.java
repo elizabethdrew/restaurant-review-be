7a20e882b8bae38292d5ed3df190eb0ba5a348ab
dreweb <elizabeth.drew@coutts.com>
2023-09-22 11:12:40 +0100

    Updated Controller Imports

--- src/main/java/dev/drew/restaurantreview/controller/AdminController.java
+++ src/main/java/dev/drew/restaurantreview/controller/AdminController.java
@@ -7,7 +7,12 @@ import org.openapitools.model.AdminStatus;
 import org.openapitools.model.ClaimStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
