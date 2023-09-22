ca545e6fcf478dba36166be943ac405f2ccc4192
dreweb <elizabeth.drew@coutts.com>
2023-09-22 14:59:03 +0100

    Updated review table

--- src/main/java/dev/drew/restaurantreview/entity/ReviewEntity.java
+++ src/main/java/dev/drew/restaurantreview/entity/ReviewEntity.java
@@ -1,14 +1,6 @@
 package dev.drew.restaurantreview.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -55,6 +47,13 @@ public class ReviewEntity extends org.openapitools.model.Review {
 
     @Column(name = "is_deleted", nullable = false)
     private Boolean isDeleted = false;
+
+    @Column(name = "reply")
+    private String reply;
+
+    @Column(name = "reply_date")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private OffsetDateTime replyDate;
 
     @Override
     public Long getId() {
